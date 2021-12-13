96012fd4ddda3b8fc63a1fbdd3b420f1b0651ec4
Neeraj Nigam <888neerajnigam@gmail.com>
2021-12-13 22:40:16 +0530

    Add files via upload

---
+++ multiplier.js
@@ -0,0 +1,13 @@
+function output(a) {
+    a = document.getElementById("num1").value;
+    return function (b) {
+        b = document.getElementById("num2").value;
+        return function (c) {
+            c = document.getElementById("num3").value;
+            alert("The result is " + a * b * c);
+            
+        }
+    }
+}
+output(a)(b)(c); 
+
